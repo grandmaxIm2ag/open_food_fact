401bddff05bfc7c51084033faa61804d28d1148d
Maxence Grand <maxencegrand1995@gmail.com>
2019-01-16 13:22:44 +0100

    ...

--- viz/main.js
+++ viz/main.js
@@ -4,7 +4,7 @@ function init() {
     var nutri = nutriscore(svg);
     var count_graph = country_graph(svg);
     var Observer=[nutri, count_graph];
-    var map = worldmap(Observer, svg); 
+    var map = worldmap(Observer, svg);
     map.draw_worldmap(dm);
     nutri.draw_bar_charts_nutriscore(dm);
     count_graph.initCountryGraph(dm);
--- viz/world_map.js
+++ viz/world_map.js
@@ -10,18 +10,18 @@ function worldmap(obs, svg){
     worldmap.path = d3.geoPath();
     
     worldmap.projection = d3.geoEquirectangular()
-            .scale(55)
-            .translate( [width / 2, height / 1.5]);
+        .scale(55)
+        .translate( [width / 2, height / 1.5]);
 
     worldmap.path.projection(worldmap.projection);        
     worldmap.svg = svg.append("svg")
-            .attr("id", "svg")
-            .attr("width", width)
-            .attr("height", height)
-            .style("background-color", 'red')
-            .attr("transform", "translate(" +
-                  (graphPosition.left +margin.left) + "," +
-                  (graphPosition.top + margin.top) + ")");
+        .attr("id", "svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("background-color", 'red')
+        .attr("transform", "translate(" +
+              (graphPosition.left +margin.left) + "," +
+              (graphPosition.top + margin.top) + ")");
     
     worldmap.draw = function (world, dm){
         worldmap.svg.append("g")
@@ -29,18 +29,20 @@ function worldmap(obs, svg){
             .selectAll("path")
             .data(world.features)
 	    .enter()
-	    .filter(
-		function(d){
-		    if(dm.choosen_filter == dm.filter.CONTINENT &&
-		       dm.choosen_region != d.properties.continent){
-			return false;
-		    }else{
-			return true;
-		    }
-		}
-	    )
-            .append("path")
-            .style("fill", function(d) { return color_continent
+	    .append("path")
+	    .style("opacity",
+		   function(d){
+		       if(dm.choosen_filter == dm.filter.CONTINENT &&
+			  dm.choosen_region != d.properties.continent){
+			   return 0.2;
+		       }else if (dm.choosen_filter == dm.filter.CONTINENT){
+			   return 1.0;
+		       }else{
+			   return 0.7;
+		       }
+		   }
+		  )
+	    .style("fill", function(d) { return color_continent
                                          (d.properties.continent); })
             .attr("d", worldmap.path)
             .on("click", function(d){
